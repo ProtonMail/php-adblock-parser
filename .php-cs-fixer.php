28f2deb2c4d9f59b0b6a1158a07a05596f66489e
Cyril van Schreven <cyril.schreven@protonmail.com>
2023-04-05 17:13:50 +0200

    Add php-cs-fixer config function

---
+++ .php-cs-fixer.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->in(__DIR__)
+    ->name('*.php');
+
+$config = new PhpCsFixer\Config();
+
+return $config->setRules([
+    '@Symfony' => true,
+    'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
+    'concat_space' => ['spacing' => 'one'],
+])->setFinder($finder);
